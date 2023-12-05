80badf9c1274fa922a19ec2d23daed585b7422fe
Jiachuan Bai <56833522+baiddd@users.noreply.github.com>
2023-12-05 19:26:12 +0100

    Update MainMenuScene.js

--- src/MainMenuScene.js
+++ src/MainMenuScene.js
@@ -7,15 +7,15 @@ export class MainMenuScene extends Phaser.Scene {
   preload() {
     // Preload your images
     this.load.image('background', 'img/bckg.jpg');
-    this.load.image('image1', 'img/lion.png');
-    this.load.image('image2', 'img/cat.png');
+    this.load.image('lion', 'img/lion.png');
+    this.load.image('cat', 'img/cat.png');
     // Add more images as needed
   }
 
   create() {
     const background = this.add.image(window.innerWidth / 2, window.innerHeight / 2, 'background');
 
-    const imageKeys = ['image1', 'image2']; // Add more image keys here
+    const imageKeys = ['lion', 'cat']; // Add more image keys here
 
     // ... (code for creating gallery of images and handling image selection)
   }
