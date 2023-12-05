e8531cb9149a3d355080be3e49b7ac8e9337e93a
Jiachuan Bai <56833522+baiddd@users.noreply.github.com>
2023-12-05 18:14:59 +0100

    Update Game.js

--- src/Game.js
+++ src/Game.js
@@ -1,1 +1,31 @@
-
+Candy.Game = function(game) {
+	this._player = null;
+	this._candyGroup = null;
+	this._spawnCandyTimer = 0;
+	this._fontStyle = null;
+	Candy._scoreText = null;
+	Candy._score = 0;
+	Candy._health = 0;
+};
+Candy.Game.prototype = {
+	create: function() {
+		// ... 
+	},
+	managePause: function() {
+		// ... 
+	},
+	update: function() {
+		// ... 
+	}
+};
+Candy.item = {
+	spawnCandy: function(game) {
+		// ... 
+	},
+	clickCandy: function(candy) {
+		// ... 
+	},
+	removeCandy: function(candy) {
+		// ... 
+	}
+};
