2a7e695f3c18248fc1bc0eec141172a1be77f7cc
baiddd <bjc4991@outlook.com>
2023-12-06 15:59:38 +0100

    change Game.js

--- src/Game.js
+++ src/Game.js
@@ -2,11 +2,22 @@ import { MainMenu } from './scenes/MainMenu.js';
 import { Coloring } from './scenes/Coloring.js';
 
 // Create a Phaser game instance
+
 const config = {
   type: Phaser.AUTO,
   width: window.innerWidth,
   height: window.innerHeight,
-  scene: [MainMenu, Coloring]
+  scale: {
+    mode: Phaser.Scale.RESIZE,
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  },
+  scene: [MainMenu, Coloring],
+  // Other configuration options...
 };
 
 const game = new Phaser.Game(config);
+game.scene.add('MainMenu', MainMenu);
+game.scene.add('ColoringGame', ColoringGame);
+
+// Start the main menu scene
+game.scene.start('MainMenu');
